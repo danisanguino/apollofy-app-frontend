9659a248369694270ed13d44fcc612d9a95c52bb
Julia Sanchis <jursanchis@gmail.com>
2024-03-01 10:03:50 +0100

    provisional routes and button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import { Buttons } from "./components/button";
 
 function App() {
   return (
     <>
       <h1>APP</h1>
+      <Buttons />
     </>
   );
 }
