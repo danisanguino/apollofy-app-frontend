62238bef8632649ed982143e477fb5c096959f05
unknown <josemari94@gmail.com>
2024-03-01 16:39:09 +0100

    form validation

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -12,6 +12,8 @@ export function Login({}: Props) {
     const [userEmail, setUserEmail] = useState('');
     const [userPass, setUserPass] = useState('');
     const [users, setUsers] = useState([] as User[]);
+    const [userError, setUserError] = useState(false);
+    const [passError, setPassError] = useState(false);
 
     useEffect(() => {
     async function getUserApi() {
@@ -22,7 +24,19 @@ export function Login({}: Props) {
 
     const loginForm = (element: React.FormEvent<HTMLFormElement>) => {
         element.preventDefault();
-       console.log(userEmail, userPass)   
+       console.log(userEmail, userPass) 
+       const foundUser = users.find((user) => user.email === userEmail)
+       if (foundUser) {
+        if (foundUser.password === userPass) {
+        console.log('correcto')
+        } else {
+            setUserError(false)
+            setPassError(true)
+        }
+       }  else {
+        setUserError(true)
+        setPassError(false)
+       }
     }
 
   return (
@@ -30,7 +44,9 @@ export function Login({}: Props) {
             <Logo/>
                 <form  className='login-buttons' onSubmit={loginForm}>
                 <Inputs  name='email' placeholder='email' handleChange={(ev) =>{setUserEmail(ev.target.value)}} />
+                {userError && <small className='message-error email'>user not found</small>}
                 <Inputs type='password' name='pass' placeholder='password' handleChange={(ev) =>{setUserPass(ev.target.value)}}/>
+                {passError && <small className='message-error password'>incorrect password</small>}
                 <Button style='btn-yellow'>LOG IN</Button>
                 </form>
     </div>
