2c13052a6ae89d6c639d47c2018ec83fdf7938fa
Julia Sanchis <jursanchis@gmail.com>
2024-05-30 13:59:30 +0200

    start implementing test

---
+++ src/components/layout/logo/__test__/index.test.tsx
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("un test estupido", () => {
+  test("frenguel", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+/// <reference types=”vitest” />
+/// <reference types=”Vite/client” />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    enviroment: "jsdom",
+  // globals: true
   },
 });
