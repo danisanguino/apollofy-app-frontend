e89ff99e3e5d66cf788ac9168bd62466bf205d50
Julia Sanchis <jursanchis@gmail.com>
2024-05-31 14:40:31 +0200

    remove use client from slider

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,9 +1,7 @@
-'use client';
-
-import * as React from 'react';
-import * as SliderPrimitive from '@radix-ui/react-slider';
-import './slider.css';
-import { cn } from '@/lib/utils';
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+import "./slider.css";
+import { cn } from "@/lib/utils";
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -11,7 +9,7 @@ const Slider = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
-    className={cn('slider1', className)}
+    className={cn("slider1", className)}
     {...props}
   >
     <SliderPrimitive.Track className="slider2">
